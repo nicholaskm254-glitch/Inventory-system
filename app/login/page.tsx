a58ea5383f846a1bdd7644b39bc2072993939c7e
Nicholas <nicholaskm254@gmail.com>
2026-06-24 11:35:42 +0300

    login update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -48,8 +48,8 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className=" p-8 rounded-xl shadow-lg w-full max-w-md">
         <h1 className="text-3xl font-bold text-center mb-6">
           Inventory System
         </h1>
