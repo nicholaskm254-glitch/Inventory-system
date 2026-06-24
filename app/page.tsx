7f42c16b6783e1d889d388b51285b77265ed6872
Nicholas <nicholaskm254@gmail.com>
2026-06-24 12:43:23 +0300

    emoji

--- app/page.tsx
+++ app/page.tsx
@@ -131,7 +131,8 @@ export default function Home() {
           {user && (
             <div className="mt-2">
               <p className="text-lg">
-                Welcome, <strong>{user.fullName}</strong>
+                Welcome
+               <strong>{user.fullName}😊</strong>
               </p>
 
               <p className="text-sm text-gray-500">
